530fa3aae68b9fb6fefdd8feb796bbc4c521911b
Maxkambek <mahkamkasimov94@gmail.com>
2023-06-10 11:48:05 +0500

    main test check

--- main/views.py
+++ main/views.py
@@ -23,26 +23,31 @@ class MainTestCheck(APIView):
     permission_classes = [IsAuthenticated]
 
     def post(self, request, *args, **kwargs):
-            ans = self.request.data['answers']
-            ans = list(ans)
-            if not ans:
-                return Response('Error', status=400)
-            lst = []
-            for i in range(1, len(ans) + 1):
-                main = MainTest.objects.filter(id=i).first()
-                if not main:
-                    return Response('Error', status=400)
-                if main.true_answer != ans[i - 1][f'{i}']:
-                    lst.append(i)
-                    te = UserQuestion.objects.filter(user=self.request.user, question_id=i)
-                    if not te:
-                        try:
-                            quest = Question.objects.get(id=i)
-                            test = UserQuestion.objects.create(user=self.request.user, question=quest, is_paid=True)
-                            test.save()
-                        except:
-                            pass
-            return Response({'message': f'Sizda {len(lst)} ta savol chiqdi'}, status=200)
+        ans = self.request.data['answers']
+        ans = list(ans)
+        if not ans:
+            return Response('Error', status=400)
+        lst = []
+        for i in range(1, len(ans) + 1):
+            if i == 1:
+                main = MainTest.objects.filter(id=2).first()
+            elif i == 2:
+                main = MainTest.objects.filter(id=2).first()
+            elif i == 3:
+                main = MainTest.objects.filter(id=5).first()
+            else:
+                main = MainTest.objects.filter(id=i + 2).first()
+            if main and main.true_answer != ans[i - 1][f'{i}']:
+                lst.append(i)
+                te = UserQuestion.objects.filter(user=self.request.user, question_id=i)
+                if not te:
+                    try:
+                        quest = Question.objects.get(id=i)
+                        test = UserQuestion.objects.create(user=self.request.user, question=quest, is_paid=True)
+                        test.save()
+                    except:
+                        pass
+        return Response({'message': f'Sizda {len(lst)} ta savol chiqdi'}, status=200)
 
 
 class QuestionListAPIView(APIView):
